477f23fd43e10bfe869d83cca62991f858b16b52
Kristoff016 <kt401876@gmail.com>
2026-09-11 12:05:05 +0200

    prog_tetelek

--- alapok2.py
+++ alapok2.py
@@ -1,4 +1,25 @@
 #Kérj be számokat * végjelig, és add őket össze
 
-#while True:
-#    adat = input("Adjon meg egy számot: (* = vége): ")
+
+# osszeg = 0
+# while True:
+#     adat = input("Adjon meg egy számot: (* = vége): ")
+#     if adat == "*":
+#         break
+#     else:
+#         osszeg += int(adat)
+
+# print(f"Az összeg: {osszeg}")
+
+
+szamok = []
+osszeg = 0
+while True:
+    adat = input("Adjon meg egy számot: (* = vége): ")
+    if adat == "*":
+        break
+    else:
+        szamok.append(adat)
+
+osszeg = sum(szamok)
+print(osszeg)
